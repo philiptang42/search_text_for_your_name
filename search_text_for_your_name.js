cbd85846f0ae4da73652efbbff0b0ecf85f71f44
philiptang42 <philiptang42@gmail.com>
2016-01-30 20:49:47 -0500

    set initial variables

---
+++ search_text_for_your_name.js
@@ -0,0 +1,9 @@
+/*jshint multistr:true */
+var text = "My name is Philip. But \
+not everyone calls me that. Some call \
+me Phil. Or Flip, as in Felip. Or Phrog.\
+...as in, Frog. \
+Either way, my name is Philip. Got it? \
+Philip. Not Frog. It's Philip.";
+var myName = "Philip";
+var hits = [];
